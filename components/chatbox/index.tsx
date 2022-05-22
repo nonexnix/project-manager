ef27fdaacb1abebf2133590d512c59d1f85b5fb8
ntabucejo <ntabucejo@gmail.com>
2022-05-22 15:04:26 +0800

    Update field store

--- components/chatbox/index.tsx
+++ components/chatbox/index.tsx
@@ -9,9 +9,7 @@ const Chatbox = () => {
   const member = useClientStore((state) => state.member)
   const project = useClientStore((state) => state.project)
   const messages = useClientStore((state) => state.messages)
-  const value = useFieldStore((state) => state.message)
-  const set = useFieldStore((state) => state.set.message)
-  const clear = useFieldStore((state) => state.clear.message)
+  const messageField = useFieldStore((state) => state.message)
   const create = useClientStore((state) => state.create.message)
 
   const { data, mutate } = useFetch<IMessage[]>('/api/message/read', project.id, {
@@ -19,6 +17,8 @@ const Chatbox = () => {
     refreshInterval: 20000,
   })
 
+  console.log(member)
+  
   useEffect(() => {
     if (JSON.stringify(messages) !== JSON.stringify(data)) {
       useClientStore.getState().read.messages(data!)
@@ -29,12 +29,12 @@ const Chatbox = () => {
 
   console.log('Chatbox Rendered', messages)
 
-  const handler = async () => {
-    if (value.text) {
-      const newData = { ...value, id: cuid(), memberId: member.id }
-      clear()
+  const handler = () => {
+    if (messageField.value.text) {
+      const newData: IMessage = { ...messageField.value, id: cuid(), member: member }
+      messageField.clear()
       mutate([...messages!, newData], false)
-      await create({
+      create({
         text: newData.text,
         memberId: member.id,
         projectId: project.id,
@@ -45,7 +45,11 @@ const Chatbox = () => {
   return (
     <div className="fixed inset-0 bg-slate-300 p-5 space-y-3 overflow-scroll">
       <div className="flex flex-col gap-2 sticky top-0 bg-slate-200">
-        <input type="text" value={value.text} onChange={(event) => set({ ...value, text: event.target.value })} />
+        <input
+          type="text"
+          value={messageField.value.text}
+          onChange={(event) => messageField.set({ ...messageField.value, text: event.target.value })}
+        />
         <button className="py-2 px-3 rounded bg-orange-500 text-white" onClick={handler}>
           Send
         </button>
@@ -55,11 +59,12 @@ const Chatbox = () => {
           <div
             key={message.id}
             className={`${
-              message.memberId === member.id
+              message.member?.id === member.id
                 ? 'bg-blue-500 text-white ml-auto text-right'
                 : 'bg-gray-500 text-white mr-auto text-left'
             } py-2 px-3 rounded`}>
             <div className="text-right">{message.text}</div>
+            <div className="text-right">{message.member?.user?.username}</div>
           </div>
         ))}
       </div>
