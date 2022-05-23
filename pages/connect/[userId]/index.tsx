7bc37e8fe82033acc9862fb0680b0c41a1d2d10c
jazztinecruz <jzztn.crz@gmail.com>
2022-05-24 01:52:24 +0800

    Fixed homepage

--- pages/connect/[userId]/index.tsx
+++ pages/connect/[userId]/index.tsx
@@ -15,6 +15,7 @@ import { SearchIcon } from '@heroicons/react/outline'
 import Button from '../../../components/button/button'
 import Project from '../../../components/project/project'
 import CreateProjectModal from '../../../components/modals/create-project'
+import JoinProjectModal from '../../../components/modals/join-project'
 
 interface IProps {
   initialUser: IUser
@@ -23,6 +24,9 @@ interface IProps {
 const Home: NextPage<IProps> = ({ initialUser }) => {
   const [ready, setReady] = useState(false)
   const user = useClientStore((state) => state.user)
+  const [isCreate, setIsCreate] = useState(false)
+  const [isJoin, setIsJoin] = useState(false)
+  const [searchInput, setSearchInput] = useState('')
 
   useEffect(() => {
     setReady(true)
@@ -31,30 +35,59 @@ const Home: NextPage<IProps> = ({ initialUser }) => {
 
   if (!ready) return <></>
 
-  console.log(user)
+  console.log(isJoin)
 
   return (
     <Foundation title="Home">
       <Layout>
-        <Header />
+        <Header
+          firstName={user.firstName[0].toUpperCase() + user.firstName.slice(1)}
+          lastName={user.lastName[0].toUpperCase() + user.lastName.slice(1)}
+        />
         <Main>
           <section>
             {/* Search , Buttons */}
             <div className="grid">
               <WhiteCard>
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 mb-14">
                   <Icon icon={<SearchIcon />} />
                   <input
                     type="string"
                     className="w-full h-full py-2 pl-4 placeholder:text-typography placeholder:text-sm md:placeholder:text-md placeholder:opacity-30 outline-none"
                     placeholder="Search Project"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
                   />
                 </div>
                 <SnowCard>
-                  <div className='grid'>
-                    <div className='ml-auto flex items-center gap-2 md:gap-6'>
-                      <Button name={'Join Project'} color={'blue'}/>
-                      <Button name={'Create New Project'} color={'pink'}/>
+                  <div className="grid">
+                    <div className="ml-auto flex items-center gap-2 md:gap-6">
+                      <Button
+                        name={'Join Project'}
+                        color={'bg-blue'}
+                        handler={() => setIsJoin(!isJoin)}
+                      />
+                      {/* {isJoin && (
+                        <JoinProjectModal
+                          handler={() => setIsJoin(!isJoin)}
+                          // userId={user?.id}
+                          // projectId={user?.members?.projectId}
+                        />
+                      )} */}
+
+                      {isJoin && (
+                        <JoinProjectModal handler={() => setIsJoin(!isJoin)} />
+                      )}
+                      <Button
+                        name={'Create New Project'}
+                        color={'bg-pink'}
+                        handler={() => setIsCreate(!isCreate)}
+                      />
+                      {isCreate && (
+                        <CreateProjectModal
+                          handler={() => setIsCreate(!isCreate)}
+                        />
+                      )}
                     </div>
                   </div>
                 </SnowCard>
@@ -62,19 +95,26 @@ const Home: NextPage<IProps> = ({ initialUser }) => {
             </div>
 
             {/* User Projects */}
-
-            <div className='grid mt-10 gap-10'>
-              <h1>All ( <span className='font-bold'>{user.members?.length}</span> )</h1>
-
-              <div className='grid grid-cols-2 md:grid-cols-3 items-center'>
-                {user.members?.map((member) => (
-                  <div key={member.id}>
-                    <Project/>
-                  </div>
-                ))}
+            <div className="grid mt-10 gap-10">
+              {/* projects count */}
+              <h1>
+                All ({' '}
+                <span className="font-bold">
+                  {user.members?.filter(({ active }) => active).length}
+                </span>
+                )
+              </h1>
+              {/* all users' project */}
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+                {searchInput === '' &&
+                  user?.members
+                    ?.filter((member) => {
+                      return member.active === true
+                    })
+                    .map((member) => (
+                      <Project key={member?.id} member={member} />
+                    ))}
               </div>
-
-              <CreateProjectModal/>
             </div>
           </section>
         </Main>
@@ -110,6 +150,15 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
           project: {
             include: {
               _count: { select: { members: true, tasks: true } },
+              members: {
+                include: {
+                  user: true,
+                  tasks: true,
+                  files: true,
+                  suggestions: true,
+                  todos: true,
+                },
+              },
             },
           },
         },
