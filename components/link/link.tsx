081f332fcf75be15cd81f746cde0805cc10ab908
jazztinecruz <jzztn.crz@gmail.com>
2022-05-24 01:54:42 +0800

    Added and fixed link component

--- components/link/link.tsx
+++ components/link/link.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link'
 interface IProps {
   name: string
   link: any
+  style ? : string
 }
-const Linker = ({ name, link }: IProps) => {
+const Linker = ({ name, link, style }: IProps) => {
   return (
     <Link href={link}>
-      <a className="text-sm md:text-md whitespace-nowrap">
-        {name}
-      </a>
+      <div className={`text-sm md:text-md whitespace-nowrap grid items-center cursor-pointer z-50 ${style}`}>
+          {name}
+      </div>
     </Link>
   )
 }
