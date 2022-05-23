b378d71d6f6e8f77a4a548581980eed2065ea362
jazztinecruz <jzztn.crz@gmail.com>
2022-05-24 01:56:16 +0800

    Update schemas and prisma

--- components/members/members.tsx
+++
@@ -1,9 +0,0 @@
-const Members = () => {
-  return (
-    <div className="w-9 h-9 -ml-4 z-50 border-[2px] border-gray-200 rounded-full grid items-center justify-center">
-      <div className="w-7 h-7 bg-blue rounded-full"></div>
-    </div>
-  )
-}
-
-export default Members
