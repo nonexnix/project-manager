0956893f25b9fddc1c5a7ef37726b9a719680048
jazztinecruz <jzztn.crz@gmail.com>
2022-05-31 17:27:40 +0800

    Added edit announcement

--- components/announcement/announcement.tsx
+++ components/announcement/announcement.tsx
@@ -1,8 +1,10 @@
 import { DotsHorizontalIcon } from "@heroicons/react/outline"
 import { useState } from "react"
 import useClientStore from "../../library/stores/client"
+import useFieldStore from "../../library/stores/field"
 import Icon from "../icon/icon"
 import Linker from "../link/link"
+import EditAnnouncement from "../modals/edit-announcement"
 
 interface IProps {
   name: string
@@ -13,6 +15,15 @@ interface IProps {
 const Announcement = ({name, description, id, date} : IProps) => {
   const [isOpen, setIsOpen] = useState(false)
   const deleteAnnouncement = useClientStore((state) => state.delete.announcement)
+  const [isEdit, setIsEdit] = useState(false)
+  const updateAnnouncementName = useClientStore(
+    (state) => state.update.announcement.name
+  )
+  const updateAnnouncementDescription = useClientStore(
+    (state) => state.update.announcement.description
+  )
+  const announcementField = useFieldStore((state) => state.suggestion)
+
 
   return (
     <div className='bg-white py-5 px-8 grid gap-8'>
@@ -32,11 +43,45 @@ const Announcement = ({name, description, id, date} : IProps) => {
             {isOpen && 
               <div className='absolute top-6 right-0 bg-white shadow-md shadow-violet'>
                 {/* Edit Announcements */}
-                <Linker
-                  name={'Edit Announcement'}
-                  link={'#'}
-                  style={'py-4 px-8 hover:bg-snow transition-all duration-300'}
-                />
+                <button onClick={() => setIsEdit(true)}>
+                  <Linker
+                    name={'Edit Announcement'}
+                    link={'#'}
+                    style={'py-4 px-8 hover:bg-snow transition-all duration-300'}
+                  />
+                </button>
+
+                {isEdit && <EditAnnouncement
+                  defaultTitle={name}
+                  titleHandler={(e: any) =>
+                    announcementField.set({
+                      ...announcementField.value,
+                      name: e.target.value,
+                    })
+                  }
+                  btnTitle={() =>
+                    updateAnnouncementName({
+                      id: id,
+                      key: 'name',
+                      value: announcementField.value.name,
+                    })
+                  }
+                  defaultDescription={description}
+                  descriptionHandler={(e: any) =>
+                    announcementField.set({
+                      ...announcementField.value,
+                      description: e.target.value,
+                    })
+                  }
+                  btnDescription={() =>
+                    updateAnnouncementDescription({
+                      id: id,
+                      key: 'description',
+                      value: announcementField.value.description,
+                    })
+                  }
+                  cancelBtn={() => setIsEdit(false)}
+                />}
   
                 {/* Delete Announcements */}
                 <button onClick={() => deleteAnnouncement({ id: id })}>
