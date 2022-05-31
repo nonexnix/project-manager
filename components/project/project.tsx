27e6730b77bb0e7147e570dbfa80b04f7423d8ac
jazztinecruz <jzztn.crz@gmail.com>
2022-05-31 17:28:48 +0800

    Added project options functionalities

--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,6 +1,10 @@
 import WhiteCard from '../card/white-card'
 import IconLabel from '../icon/icon-with-label'
-import { CollectionIcon, SpeakerphoneIcon } from '@heroicons/react/outline'
+import {
+  CollectionIcon,
+  MenuIcon,
+  SpeakerphoneIcon,
+} from '@heroicons/react/outline'
 import { PencilAltIcon } from '@heroicons/react/outline'
 import { FolderOpenIcon } from '@heroicons/react/outline'
 import { LightBulbIcon } from '@heroicons/react/outline'
@@ -9,77 +13,175 @@ import Profile from '../members/profile'
 import phase from '../../library/utilities/phase'
 import Link from 'next/link'
 import { IMember } from '../../library/schemas/interfaces'
+import Linker from '../link/link'
+import Router, { useRouter } from 'next/router'
+import { useState } from 'react'
+import useClientStore from '../../library/stores/client'
+import Icon from '../icon/icon'
 
 interface IProps {
   member: IMember
 }
 
 const Project = ({ member }: IProps) => {
+  const [isOpenOption, setIsOpenOption] = useState(false)
+  const deleteProject = useClientStore((state) => state.delete.project)
+  const completeProject = useClientStore((state) => state.update.project.over)
+  const updateMembership = useClientStore(
+    (state) => state.update.member?.active
+  )
+
   return (
-    <Link href={`/connect/${member?.userId}/view/${member.id}/dashboard`}>
-      <div className="lg:w-[500px] cursor-pointer hover:-translate-y-2 transiton-all duration-500">
-        <WhiteCard>
-          <div className="grid gap-4">
-            {/* name */}
-            <div className="text-md md:text-xl leading-relaxed font-semibold">
-              {member?.project?.name}
-            </div>
+    <div>
+      <Link href={`/connect/${member?.userId}/view/${member.id}/dashboard`}>
+        <div className="lg:w-[500px] cursor-pointer hover:-translate-y-2 transiton-all duration-500">
+          <WhiteCard>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-[1fr,auto] items-center">
+                {/* name */}
+                <div className="text-md md:text-lg font-medium">
+                  {member?.project?.name}
+                </div>
 
-            {/* projectmember members */}
-            <div className="flex items-center gap-3 ml-4">
-              {member?.project?.members?.map((member) => (
-                <Profile key={member?.id} />
-              ))}
-              {member?.project!.members!.length > 5 && (
-                <h1 className="text-sm ml-5 text-gray-500">
-                  + {member?.project!.members!.length - 5} more
-                </h1>
-              )}
-            </div>
+                <button onClick={() => setIsOpenOption(!isOpenOption)}>
+                  <Icon icon={<MenuIcon />} />
+                </button>
+                {isOpenOption && (
+                  <div className="absolute top-11 right-7 bg-white shadow-md shadow-violet grid z-50">
+                    {/* Set as done */}
+                    {member!.project!.over === true ? (
+                      <button
+                        onClick={() =>
+                          completeProject({
+                            id: member!.project!.id,
+                            key: 'over',
+                            value: false,
+                          })
+                        }
+                      >
+                        <Linker
+                          name={'Set as Incomplete'}
+                          link={'#'}
+                          style={
+                            'py-4 px-8 hover:bg-snow transition-all duration-300'
+                          }
+                        />
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() =>
+                          completeProject({
+                            id: member!.project!.id,
+                            key: 'over',
+                            value: true,
+                          })
+                        }
+                      >
+                        <Linker
+                          name={'Set as Done'}
+                          link={'#'}
+                          style={
+                            'py-4 px-8 hover:bg-snow transition-all duration-300'
+                          }
+                        />
+                      </button>
+                    )}
 
-            {/* user's contribution */}
-            <div className="grid grid-flow-col items-center mb-12">
-              <IconLabel
-                icon={<CollectionIcon />}
-                label={member?.project?.files?.length}
-              />
-              <IconLabel
-                icon={<FolderOpenIcon />}
-                label={member?.project?.files?.length}
-              />
-              <IconLabel
-                icon={<SpeakerphoneIcon />}
-                label={member?.project?.announcements?.length}
-              />
-              <IconLabel
-                icon={<LightBulbIcon />}
-                label={member?.project?.suggestions?.length}
-              />
-            </div>
-          </div>
+                    {/* Delete Project */}
+                    <button
+                      onClick={() => {
+                        deleteProject({ id: member!.project!.id })
+                        Router.push(`/connect/${member!.user!.id}`)
+                      }}
+                    >
+                      <Linker
+                        name={'Delete Project'}
+                        link={'#'}
+                        style={
+                          'py-4 px-8 hover:bg-snow transition-all duration-300'
+                        }
+                      />
+                    </button>
 
-          <SnowCard>
-            <div className="grid grid-cols-[1fr,auto] items-center">
-              {/* start and due date */}
-              <div className="text-sm tracking-wide">
-                {String(phase(member!.project!.createdAt, 'LL'))} -{' '}
-                {String(phase(member!.project!.dueAt, 'LL'))}
+                    {/* Leave Project */}
+                    <button
+                      onClick={() => {
+                        updateMembership({
+                          id: member?.id,
+                          key: 'active',
+                          value: false,
+                        })
+                        Router.push(`/connect/${member!.user!.id}`)
+                      }}
+                    >
+                      <Linker
+                        name={'Leave Project'}
+                        link={'#'}
+                        style={
+                          'py-4 px-8 hover:bg-snow transition-all duration-300'
+                        }
+                      />
+                    </button>
+                  </div>
+                )}
               </div>
-              {/* completeness */}
-              {member.project?.over === true ? (
-                <h1 className="text-green-600 font-bold tracking-wide">
-                  Completed
-                </h1>
-              ) : (
-                <h1 className="text-red-600 font-bold tracking-wide">
-                  Incomplete
-                </h1>
-              )}
+
+              {/* projectmember members */}
+              <div className="flex items-center gap-3 ml-4">
+                {member?.project?.members?.map((member) => (
+                  <Profile key={member?.id} />
+                ))}
+                {member?.project!.members!.length > 5 && (
+                  <h1 className="text-sm ml-5 text-gray-500">
+                    + {member?.project!.members!.length - 5} more
+                  </h1>
+                )}
+              </div>
+
+              {/* user's contribution */}
+              <div className="grid grid-flow-col items-center mb-12">
+                <IconLabel
+                  icon={<CollectionIcon />}
+                  label={member?.project?.files?.length}
+                />
+                <IconLabel
+                  icon={<FolderOpenIcon />}
+                  label={member?.project?.files?.length}
+                />
+                <IconLabel
+                  icon={<SpeakerphoneIcon />}
+                  label={member?.project?.announcements?.length}
+                />
+                <IconLabel
+                  icon={<LightBulbIcon />}
+                  label={member?.project?.suggestions?.length}
+                />
+              </div>
             </div>
-          </SnowCard>
-        </WhiteCard>
-      </div>
-    </Link>
+
+            <SnowCard>
+              <div className="grid grid-cols-[1fr,auto] items-center">
+                {/* start and due date */}
+                <div className="text-sm tracking-wide">
+                  {String(phase(member!.project!.createdAt, 'LL'))} -{' '}
+                  {String(phase(member!.project!.dueAt, 'LL'))}
+                </div>
+                {/* completeness */}
+                {member.project?.over === true ? (
+                  <h1 className="text-green-600 font-bold tracking-wide">
+                    Completed
+                  </h1>
+                ) : (
+                  <h1 className="text-red-600 font-bold tracking-wide">
+                    Incomplete
+                  </h1>
+                )}
+              </div>
+            </SnowCard>
+          </WhiteCard>
+        </div>
+      </Link>
+    </div>
   )
 }
 
