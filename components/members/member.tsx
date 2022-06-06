02132cb134808d8a1a2f12de56da21a9188f3baa
jazztinecruz <jzztn.crz@gmail.com>
2022-06-06 23:06:52 +0800

    Add edit roles

--- components/members/member.tsx
+++ components/members/member.tsx
@@ -1,24 +1,31 @@
 import { DotsHorizontalIcon } from '@heroicons/react/outline'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { IMember, IProject, IUser } from '../../library/schemas/interfaces'
 import Icon from '../icon/icon'
 import Linker from '../link/link'
 import Role from '../role/role'
 import Profile from './profile'
-import type { GetStaticPaths, GetStaticProps } from 'next'
-import objectified from '../../library/utilities/objectified'
 import useClientStore from '../../library/stores/client'
+import GiveAuthorization from '../modals/create-authorization'
 
 interface IProps {
   user: IUser
   project: IProject
-  member:IMember
+  member: IMember
   memberId: string
   lastName: string
   firstName: string
 }
-const Member = ({ user, project,member, memberId, lastName, firstName}: IProps) => {
+const Member = ({
+  user,
+  project,
+  member,
+  memberId,
+  lastName,
+  firstName,
+}: IProps) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [giveAuthorization, setGiveAuthorization] = useState(false)
   const kickMember = useClientStore((state) => state.update.member.active)
 
   return (
@@ -33,67 +40,56 @@ const Member = ({ user, project,member, memberId, lastName, firstName}: IProps) 
         </div>
 
         {/* option button */}
-        {project?.members!.map((member) => {
-          return member?.authorizations!.map((authorization) => {
-            console.log(authorization.role?.permission)
-            if (authorization.role?.permission?.everything === true) {
-              return (
-                <div className="relative">
-                  <button onClick={() => setIsOpen(!isOpen)}>
-                    <Icon icon={<DotsHorizontalIcon />} />
-                  </button>
+        <div className="relative">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <Icon icon={<DotsHorizontalIcon />} />
+          </button>
 
-                  {isOpen && (
-                    <div className="absolute top-6 right-0 bg-white shadow-md shadow-violet">
-                      {/* Edit roles */}
-                      <Linker
-                        name={'Edit Roles'}
-                        link={'#'}
-                        style={
-                          'py-4 px-8 hover:bg-snow transition-all duration-300'
-                        }
-                      />
+          {isOpen && (
+            <div className="absolute top-6 right-0 bg-white shadow-md shadow-violet">
+              {/* Edit Authorization */}
+              <button onClick={() => setGiveAuthorization(!giveAuthorization)}>
+                <Linker
+                  name={'Edit Roles'}
+                  link={'#'}
+                  style={'py-4 px-8 hover:bg-snow transition-all duration-300'}
+                />
+              </button>
 
-                      {/* Edit Permission */}
-                      <Linker
-                        name={'Edit Permission'}
-                        link={'#'}
-                        style={
-                          'py-4 px-8 hover:bg-snow transition-all duration-300'
-                        }
-                      />
+              {giveAuthorization && (
+                <GiveAuthorization
+                  project={project}
+                  cancelBtn={() => setGiveAuthorization(!giveAuthorization)}
+                  memberId={memberId}
+                  authorizations={member.authorizations!}
+                />
+              )}
 
-                      {/* Kick Member */}
-                      <button
-                        onClick={() =>
-                          kickMember({
-                            id: memberId,
-                            key: 'active',
-                            value: false,
-                          })
-                        }
-                      >
-                        <Linker
-                          name={'Kick Member'}
-                          link={'#'}
-                          style={
-                            'py-4 px-8 hover:bg-snow transition-all duration-300'
-                          }
-                        />
-                      </button>
-                    </div>
-                  )}
-                </div>
-              )
-            }
-          })
-        })}
+              {/* Kick Member */}
+              <button
+                onClick={() =>
+                  kickMember({
+                    id: memberId,
+                    key: 'active',
+                    value: false,
+                  })
+                }
+              >
+                <Linker
+                  name={'Kick Member'}
+                  link={'#'}
+                  style={'py-4 px-8 hover:bg-snow transition-all duration-300'}
+                />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
 
       {/* roles */}
       <div className="flex items-center">
         {member.authorizations?.map((authorization) => (
-          <Role key={authorization.id} role={authorization.role?.name}/>
+          <Role key={authorization.id} role={authorization.role?.name} />
         ))}
       </div>
     </div>
@@ -101,5 +97,3 @@ const Member = ({ user, project,member, memberId, lastName, firstName}: IProps) 
 }
 
 export default Member
-
-
---
+++ components/modals/create-authorization.tsx
@@ -0,0 +1,126 @@
+import { UserIcon } from '@heroicons/react/outline'
+import React, { useState } from 'react'
+import {
+  IAuthorization,
+  IMember,
+  IProject,
+} from '../../library/schemas/interfaces'
+import useClientStore from '../../library/stores/client'
+import Icon from '../icon/icon'
+
+interface IProps {
+  project: IProject
+  cancelBtn: any
+  memberId: string
+  authorizations: IAuthorization[]
+}
+const GiveAuthorization = ({
+  project,
+  cancelBtn,
+  memberId,
+  authorizations,
+}: IProps) => {
+  const createAuthorization = useClientStore(
+    (state) => state.create.authorization
+  )
+
+  const deleteAuthorization = useClientStore(
+    (state) => state.delete.authorization
+  )
+
+  const modifiedRoles = (authorizations: IAuthorization[]) => {
+    const updatedRoles = project.roles!.map((role) => {
+      for (const authorization of authorizations) {
+        if (authorization.role?.name === role.name) {
+          return { ...role, checked: true, authorization}
+        }
+      }
+      
+      for (const authorization of authorizations) {
+        if (authorization.role?.name !== role.name) {
+          return { ...role, checked: false, authorization}
+        }
+      }
+
+      return { ...role, checked: false, authorization:{id:"", memberId:"", roleId: ""}}
+    })
+    return updatedRoles
+  }
+
+  const [roles, setRoles] = useState(modifiedRoles(authorizations))
+
+  console.log(roles)
+
+  // map project roles
+  // return Roles
+  // for loop of authorizations[]
+  // if authorization in authorizations === project roles
+  // return role if checked
+
+  return (
+    <div className="fixed inset-0">
+      <div className="bg-white max-w-5xl relative inset-2/4 -translate-x-1/2 -translate-y-1/2 shadow-xl shadow-violet px-10 py-6 z-50 grid">
+        {/* title logo*/}
+        <div className="flex items-center gap-3">
+          <Icon icon={<UserIcon />} />
+          <h1 className="text-md font-semibold">Member Roles</h1>
+        </div>
+
+        {/* PROJECT ROLES */}
+        <div className="grid gap-3 mt-6">
+          {roles.map((role) => {
+            if (role.name !== 'Member') {
+              return (
+                <div
+                  key={role.id}
+                  className="flex items-center gap-5 py-4 px-6 border-[1px] border-gray-200"
+                >
+                  <input
+                    type="checkbox"
+                    checked={role.checked}
+                    onChange={() => {
+                      const updatedRoles = roles.map((r) => {
+                        if (r.checked) {
+                          if (role.id === r.id) {
+                            deleteAuthorization({id: role.authorization.id})
+                            return { ...r, checked: false }
+                          }
+                        }
+                        if (role.id === r.id) {
+                          createAuthorization({memberId:memberId, roleId:role.id})
+                          return { ...r, checked: true }
+                        } 
+                        return r
+                      })
+                      setRoles(updatedRoles!)
+                    }}
+                  />
+                  <h1 className="text-sm">{role!.name}</h1>
+                </div>
+              )
+            }
+          })}
+        </div>
+
+        {/* buttons */}
+        <div className="flex gap-3 ml-auto mt-6">
+          <button
+            onClick={cancelBtn}
+            className="border-[1px] border-gray-200 rounded-md bg-transparent px-4 py-2  hover:text-red-500 transition-all duration-300 hover:scale-105"
+          >
+            Cancel
+          </button>
+
+          <button
+            onClick={cancelBtn}
+            className="bg-blue text-white rounded-md px-4 py-2 transition-all duration-300 hover:scale-105"
+          >
+            Done
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GiveAuthorization
