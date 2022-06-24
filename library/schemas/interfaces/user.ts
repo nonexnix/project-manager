427af5c73264103848d6a33259c637969b56a3d1
ntabucejo <ntabucejo@gmail.com>
2022-06-24 22:24:55 +0800

    Update header connection to the database

--- library/schemas/interfaces/user.ts
+++ library/schemas/interfaces/user.ts
@@ -1,19 +1,20 @@
-import IMember from './member'
-import IProject from './project'
-import ITicket from './ticket'
+import IMember from "./member";
+import IProject from "./project";
+import ITicket from "./ticket";
 
 interface IUser {
-  id: string
-  email: string
-  username: string
-  firstName: string
-  lastName: string
-  image: string
-  createdAt: string | Date
-  updatedAt: string | Date
-  members?: IMember[]
-  projects?: IProject[]
-  tickets?: ITicket[]
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  image: string;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  members?: IMember[];
+  projects?: IProject[];
+  tickets?: ITicket[];
 }
 
-export default IUser
+export default IUser;
